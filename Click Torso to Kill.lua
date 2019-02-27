d26f6e5c9542eef64bdc3c30bcc150644bc6487a
Gideon Lingle <gideon.lingle1@gmail.com>
2019-02-26 20:58:14 -0600

    Update Click Torso to Kill.lua

--- Click Torso to Kill.lua
+++ Click Torso to Kill.lua
@@ -1,31 +1,10 @@
-print("Click-to-Kill.lua by FreakingHulk is loaded!")
-
-name = "FreakingHulk" -- Your name
-clicker = Instance.new("ClickDetector")
-clicker.Name = "TROLLED :)"
-player = game.Players[name]
-
-game.Players.PlayerAdded:Connect(function(p)
-  p.CharacterAdded:Connect(function(char)
-    if p ~= player then
+for _,v in next, game.Players:GetPlayers() do
+  v.CharacterAdded:Connect(function(char)
+      local m = Instance.new("ClickDetector", char.HumanoidRootPart)
+      m.Name = "Trolled :)"
       
-      clicker.MouseClick:Connect(function(plr)
+      m.MouseClick:Connect(function()
         char.Humanoid:TakeDamage(100)
       end)
-    else
-      return
-    end
   end)
-end)
-
-for _,v in next, game.Players:GetPlayers() do
-  local char = v.Character
-  
-  if v ~= player then
-    clicker.MouseClick:Connect(function(plr)
-      char.Humanoid:TakeDamage(100)
-    end)
-  else
-    return
-  end
 end
